621628ad2dcf0b395821fec73f21050d555c8894
meenakshisundaram-in <109644642+meenakshisundaram-in@users.noreply.github.com>
2022-09-28 10:12:59 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,19 @@
-n = int(input())
-arr=[]
-
-for x in range(0,n):
-    a = int(input())
-    arr.append(a)
-
-for x in range(0,len(arr)):
-  smallest = arr[x]
-  for y in range(x+1,len(arr)):
-    if(arr[x]>arr[y]):
-      smallest = y
-      arr[x], arr[smallest] = arr[smallest],arr[x]
-
-print(arr)
+def selectionSort(array, size)->list:
+   
+    for i in range(0,size):
+        smallest = array[i]
+        for x in range(i+1,size):
+            if array[i]>array[x]:
+                smallest = x
+                array[smallest],array[i] = array[i],array[smallest]
+    return array
+               
+# Do not change the following code
+input_data = input()
+data = []
+for item in input_data.split(','):
+  if item.isnumeric():
+    data.append(int(item))
+  elif item.lstrip("-").isnumeric():
+    data.append(int(item))
+print(selectionSort(data, len(data)))
