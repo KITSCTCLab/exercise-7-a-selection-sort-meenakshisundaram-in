39342c2f20e83ee14b239ea8899a15fb6bc24847
meenakshisundaram-in <109644642+meenakshisundaram-in@users.noreply.github.com>
2022-09-28 10:07:53 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,15 @@
+n = int(input())
+arr=[]
+
+for x in range(0,n):
+    a = int(input())
+    arr.append(a)
+
 for x in range(0,len(arr)):
   smallest = arr[x]
   for y in range(x+1,len(arr)):
     if(arr[x]>arr[y]):
-      smallest = arr[y]
-  arr[x], arr[smallest] = arr[smallest],arr[x]
+      smallest = y
+      arr[x], arr[smallest] = arr[smallest],arr[x]
+
+print(arr)
